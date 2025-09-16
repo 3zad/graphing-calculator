8a59ea2fe4b8521b099978433d2778b812fb540b
Zachary Davis <test106437@gmail.com>
2025-09-16 10:20:29 +0300

    temp

--- source/components/grid.d
+++ source/components/grid.d
@@ -18,13 +18,16 @@ void grid()
 
     // y-axis
     graphRectangle(0, -s.offsetY/100, 2, GetScreenHeight()-s.gridThickness*GetScreenHeight(), Colors.WHITE);
+
     // x-axis
     graphRectangle(s.offsetX/100, 0, GetScreenWidth()-s.gridThickness*GetScreenWidth(), 2, Colors.WHITE);
+
+    graphRectangle(50, 10, 20, 20, Colors.WHITE);
     
     // Markers and grid lines
     for (int z = -1; z < 2; z+=2) {
         int markerPos = 0;
-        while (markerPos < GetScreenWidth()-s.offsetX && markerPos > -GetScreenWidth()-s.offsetX) {
+        while (markerPos < GetScreenWidth() && markerPos > -GetScreenWidth()) {
             graphRectangle(markerPos, 0, 1, GetScreenHeight(), Colors.GRAY);
             graphRectangle(markerPos, 0, 2, 50, Colors.RED);
             markerPos += z;
@@ -33,7 +36,7 @@ void grid()
 
     for (int z = -1; z < 2; z+=2) {
         int markerPos = 0;
-        while (markerPos < GetScreenHeight()-s.offsetY && markerPos > -GetScreenHeight()-s.offsetY) {
+        while (markerPos < GetScreenHeight() && markerPos > -GetScreenHeight()) {
             graphRectangle(0, markerPos, GetScreenWidth(), 1, Colors.GRAY);
             graphRectangle(0, markerPos, 50, 2, Colors.RED);
             markerPos += z;
